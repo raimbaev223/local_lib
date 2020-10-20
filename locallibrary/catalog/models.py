bdd5815fa2b225825de64df9a5089c1015114cd3
Er.Py <er.py@MacBook-Pro-Admin.local>
2020-10-20 19:13:46 +0600

    commit

---
+++ locallibrary/catalog/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text="Введите жанр книги",
+        verbose_name='Жанр книги',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name='название книги')
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name='Автор')
+    summary = models.TextField(max_length=1000, help_text="описание книги", verbose_name='описание')
+    genre = models.ManyToManyField(Genre, help_text="выберите жанр", verbose_name='Жанр')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Жанр'
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self):
+        return '%s (%s)' % (self.id, self.book.title)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
